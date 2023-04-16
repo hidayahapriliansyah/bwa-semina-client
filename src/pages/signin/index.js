b83ed08deb7b2e89581566b3542c5aa0d4594efa
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-16 13:32:23 +0700

    setup-project-react-6

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,33 +1,71 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import { Card, Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
+import SButton from '../../components/Button';
+import SAlert from '../../components/Alert';
 
 function PageSignin() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    console.log({
+      email: form.email,
+      password: form.password,
+    });
+    try {
+      const res = await axios.post(
+        'http://localhost:9000/api/v1/cms/auth/signin',
+        {
+          email: form.email,
+          password: form.password,
+        }
+      );
+
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container md={12}>
+      <SAlert message={'Hello'} type={'info'} />
       <Card style={{ width: '50%' }} className='m-auto mt-5 p-2'>
         <Card.Body>
           <Card.Title className='text-center'>Form Signin</Card.Title>
         </Card.Body>
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+          <TextInputWithLabel 
+            label='Email address'
+            name="email"
+            value={form.email}
+            type="email"
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+          <TextInputWithLabel 
+            label='Password'
+            name="password"
+            value={form.password}
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+
+          <SButton action={handleSubmit} variant="primary" type="submit">
+          Submit
+          </SButton>
         </Form>
       </Card>
     </Container>
