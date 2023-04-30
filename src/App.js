77351e02bed346437ff3a2e7faf000085bfdcb71
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-30 11:21:20 +0700

    redux-role-access-part-3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Routes,
-  Route,
 } from 'react-router-dom';
-import PageSignin from './pages/signin';
 import './App.css';
-import Dahsboard from './pages/dashboard';
-import PageCategories from './pages/categories';
-import CategoryEdit from './pages/categories/edit';
-import CategoryCreate from './pages/categories/create';
 import { useEffect } from 'react';
 import { listen } from './redux/listener';
+import { AppRoutes } from './routes';
 
 function App() {
   useEffect(() => {
@@ -20,13 +14,7 @@ function App() {
   }, []);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Dahsboard />} />
-        <Route path='/signin' element={<PageSignin />} />
-        <Route path='/categories' element={<PageCategories />} />
-        <Route path='/categories/create' element={<CategoryCreate />} />
-        <Route path='/categories/edit/:id' element={<CategoryEdit />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import NavLink from '../NavLink';
+import NavAccess from '../NavAccess';
 import { useNavigate } from 'react-router-dom';
 
 export default function SNavbar() {
@@ -11,12 +11,12 @@ export default function SNavbar() {
     <Container>
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
       <Nav className="me-auto">
-        <NavLink action={() => navigate('/')}>Home</NavLink>
-        <NavLink action={() => navigate('/categories')}>Categories</NavLink>
-        <NavLink action={() => navigate('/talents')}>Talents</NavLink>
-        <NavLink action={() => navigate('/events')}>Events</NavLink>
-        <NavLink action={() => navigate('/participants')}>Participants</NavLink>
-        <NavLink action={() => navigate('/transactions')}>Transactions</NavLink>
+        <NavAccess action={() => navigate('/')}>Home</NavAccess>
+        <NavAccess action={() => navigate('/categories')}>Categories</NavAccess>
+        <NavAccess action={() => navigate('/talents')}>Talents</NavAccess>
+        <NavAccess action={() => navigate('/events')}>Events</NavAccess>
+        <NavAccess action={() => navigate('/participants')}>Participants</NavAccess>
+        <NavAccess action={() => navigate('/transactions')}>Transactions</NavAccess>
       </Nav>
     </Container>
   </Navbar>
