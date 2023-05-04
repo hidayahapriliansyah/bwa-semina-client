30aca2ac6f51facca374de5733d3cdba010f9687
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-04 09:33:05 +0700

    payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import Login from '../pages/signin';
 import { HomeRoute } from './HomeRoute';
 import { TalentsRoute } from './TalentsRoute';
 import { CategoriesRoute } from './CategoriesRoute';
-// import { PaymentsRoute } from './PaymentsRoute';
+import { PaymentsRoute } from './PaymentsRoute';
 import SNavbar from '../components/Navbar';
 
 export function AppRoutes() {
@@ -32,7 +32,7 @@ export function AppRoutes() {
         <Route path='dashboard/*' element={<HomeRoute />} />
         <Route path='categories/*' element={<CategoriesRoute />}/>
         <Route path='talents/*' element={<TalentsRoute />}/>
-        {/* <Route path='payments/*' element={<PaymentsRoute />}/> */}
+        <Route path='payments/*' element={<PaymentsRoute />}/>
         <Route path='' element={<Navigate to='/dashboard' replace={true} />} />
       </Route>
     </Routes>
